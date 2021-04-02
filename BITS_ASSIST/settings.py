ff201d6b201c3db13156e80b1b8499f8a994f1eb
dhruvpopuri <80945440+dhruvpopuri@users.noreply.github.com>
2021-04-02 11:38:30 +0530

    Update settings.py

--- BITS_ASSIST/settings.py
+++ BITS_ASSIST/settings.py
@@ -20,8 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a$n)odr=kk6d5uotgzm@@)t108$n68xl1!%u*7eh%p^pp8hy_!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
